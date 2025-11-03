3ab95012eab71fd31b804534285cb6a79085a846
juliolisboa-lab <julio.castro.116999@a.fecaf.com.br>
2025-11-03 12:03:46 -0300

    Delete src/routes/tests/test_tarefa.py

--- src/routes/tests/test_tarefa.py
+++
@@ -1,9 +0,0 @@
-# tests/test_tarefa.py
-from src.controllers.tarefa_controller import criar_tarefa, listar_tarefas
-
-def test_criar_tarefa():
-    tarefas = listar_tarefas()
-    total_antes = len(tarefas)
-    criar_tarefa("Testar função", "Descrição de teste")
-    tarefas_depois = listar_tarefas()
-    assert len(tarefas_depois) == total_antes + 1
